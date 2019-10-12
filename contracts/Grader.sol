9954a2caeb3d31b00f1d70f9834e5808da833be7
saiteja-talluri <saitejatalluri836@gmail.com>
2019-10-12 13:17:22 +0530

    Updated contract

--- contracts/Grader.sol
+++ contracts/Grader.sol
@@ -36,6 +36,8 @@ contract Grader {
         mapping (bytes32 => bool) examIds;
         mapping (bytes32 => Exam) exams;
         mapping (bytes32 => uint256) weightage;
+        mapping (bytes32 => uint256) totalMarks;
+        mapping (bytes32 => bytes32) grades;
     }
 
     mapping(bytes32 => Course) public courses;
@@ -90,23 +92,63 @@ contract Grader {
             return false;
     }
 
-    function setWeightages(bytes32 courseID, mapping (bytes32 => uint256) weightage) {
+    function setWeightages(bytes32 courseID, mapping (bytes32 => uint256) weightage) public returns (bool added)  {
+        if (courseIds[courseID] && instructors[msg.sender]) {
+            courses[courseID].weightage = weightage;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function setGradeCutoffs (bytes32 courseID, uint256[] gradeCutoffs) public returns (bool added) {
+        if (courseIds[courseID] && instructors[msg.sender]) {
+            bytes32[] gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
+            if (gradeCutoffs.length != (gradeList.length - 1))
+                return false;
+            courses[courseID].gradeCutoffs = gradeCutoffs;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function calculateTotal (bytes32 courseID, uint16 precision) public returns (bool added) {
+        if (courseIds[courseID] && instructors[msg.sender]) {
+            uint256 pres = 10**precision;
+            for (uint256 i = 0; i < courses[courseID].examIDList.length; i++) {
+                bytes32 exam_id = courses[courseID].examIDList[i]
+                uint256 maxmarks = courses[courseID].exams[exam_id].maxMarks;
+                uint256 weightage = courses[courseID].weightage[exam_id];
+                for (uint256 j = 0; j < courses[courseID].rollList.length; j++) {
+                      bytes32 roll_no = courses[courseID].rollList[j];
+                      courses[courseID].totalMarks[roll_no] += ((courses[courseID].exams[exam_id].marks[roll_no] * pres * weightage)/maxmarks);
+                }
+            }
+            for (uint256 k = 0; k < courses[courseID].rollList.length; k++) {
+                  bytes32 roll_no = courses[courseID].rollList[k];
+                  courses[courseID].totalMarks[roll_no] = (courses[courseID].totalMarks[roll_no]/pres);
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function calculateGrades (bytes32 courseID) public returns (bool added) {
       if (courseIds[courseID] && instructors[msg.sender]) {
-          courses[courseID].weightage = weightage;
+          bytes32[] gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
+          for (uint256 i = 0; i < courses[courseID].rollList.length; i++) {
+              for (uint256 j = 0; j < courses[courseID].gradeCutoffs.length; j++) {
+                    bytes32 roll_no = courses[courseID].rollList[i];
+                    if (courses[courseID].totalMarks[roll_no] >= courses[courseID].gradeCutoffs[j])
+                        courses[courseID].grades[roll_no] = gradeList[j];
+                        break;
+              }
+          }
           return true;
       }
       else
           return false;
     }
-
-    function setGradeCutoffs (bytes32 courseID, uint256[] gradeCutoffs) {
-      if (courseIds[courseID] && instructors[msg.sender]) {
-          courses[courseID].gradeCutoffs = gradeCutoffs;
-          return true;
-      }
-      else
-          return false;
-    }
-
-    function cal
 }
