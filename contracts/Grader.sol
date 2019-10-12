00a781ae1cd3eaafbda8e7a5e8b1bacaf9d562a9
Pavan Bhargav <tangiralapavan1999@gmail.com>
2019-10-13 01:36:02 +0530

    updated sol

--- contracts/Grader.sol
+++ contracts/Grader.sol
@@ -13,15 +13,14 @@ contract Grader {
     }
 
     function kill() public {
-        if(msg.sender == admin) {
-            selfdestruct(admin);
-        }
+        require(msg.sender == admin);
+        selfdestruct(admin);
     }
 
     struct Exam {
         bytes32 examID;
-        uint256 maxMarks;
-        mapping (bytes32 => uint256) marks;
+        uint maxMarks;
+        mapping (bytes32 => uint) marks;
     }
 
     struct Course {
@@ -30,130 +29,109 @@ contract Grader {
         bytes32[] rollList;
         address instructor;
         bytes32[] examIDList;
-        uint256[] gradeCutoffs;
+        uint[] gradeCutoffs;
         mapping (address => bool) TAs;
         mapping (bytes32 => bool) examIds;
         mapping (bytes32 => Exam) exams;
-        mapping (bytes32 => uint256) weightage;
-        mapping (bytes32 => uint256) totalMarks;
+        mapping (bytes32 => uint) weightage;
+        mapping (bytes32 => uint) totalMarks;
         mapping (bytes32 => string) grades;
     }
 
     mapping(bytes32 => Course) courses;
 
     function addInstructor(address[] instrlist) public returns (bool added) {
-        if (msg.sender == admin) {
-            for (uint256 i = 0; i < instrlist.length; i++)
+        require (msg.sender == admin, "addInstructor");
+        for (uint i = 0; i < instrlist.length; i++){
+            if(!instructors[instrlist[i]]){
                 instructorsList.push(instrlist[i]);
                 instructors[instrlist[i]] = true;
-            return true;
+            }
         }
-        else
-            return false;
+        added = true;
     }
 
     function addCourse(bytes32 courseID, string courseName, bytes32[] rollList, address[] TAs) public returns (bool added) {
-        if (instructors[msg.sender] && (!courseIds[courseID])) {
-            bytes32[] memory examIDList;
-            uint256[] memory gradeCutoffs;
-            courseInstructor[courseID] = msg.sender;
-            courseIds[courseID] = true;
-            courseIDList.push(courseID);
-            courses[courseID] = Course(courseID, courseName, rollList, msg.sender, examIDList, gradeCutoffs);
-            for (uint256 i = 0; i < TAs.length; i++)
-                    courses[courseID].TAs[TAs[i]] = true;
-            return true;
-        }
-        else
-            return false;
+        require (instructors[msg.sender] && (!courseIds[courseID]), "addCourse");
+        bytes32[] memory examIDList;
+        uint[] memory gradeCutoffs;
+        courseInstructor[courseID] = msg.sender;
+        courseIds[courseID] = true;
+        courseIDList.push(courseID);
+        courses[courseID] = Course(courseID, courseName, rollList, msg.sender, examIDList, gradeCutoffs);
+        for (uint i = 0; i < TAs.length; i++)
+                courses[courseID].TAs[TAs[i]] = true;
+        added = true;
     }
 
-    function addExam(bytes32 courseID, bytes32 examID, uint256 maxMarks, bytes32[] rollList, uint256[] marksList) public returns (bool added) {
-        if (courseIds[courseID] && (instructors[msg.sender] || courses[courseID].TAs[msg.sender])) {
-            if (marksList.length != rollList.length)
-                return false;
-            courses[courseID].examIds[examID] = true;
-            courses[courseID].examIDList.push(examID);
-            courses[courseID].exams[examID] = Exam(examID, maxMarks);
-            for (uint256 i = 0; i < rollList.length; i++)
-                courses[courseID].exams[examID].marks[rollList[i]] = marksList[i];
-            return true;
-        }
-        else
-            return false;
+    function addExam(bytes32 courseID, bytes32 examID, uint maxMarks, bytes32[] rollList, uint[] marksList) public returns (bool added) {
+        require (courseIds[courseID] && (instructors[msg.sender] || courses[courseID].TAs[msg.sender]) && (!courses[courseID].examIds[examID]), "addExam");
+        if (marksList.length != rollList.length)
+            added = false;
+        courses[courseID].examIds[examID] = true;
+        courses[courseID].examIDList.push(examID);
+        courses[courseID].exams[examID] = Exam(examID, maxMarks);
+        for (uint i = 0; i < rollList.length; i++)
+            courses[courseID].exams[examID].marks[rollList[i]] = marksList[i];
+        added = true;
     }
 
-    function updateMarks(bytes32 courseID, bytes32 examID, bytes32[] rollList, uint256[] marksList) public returns (bool added) {
-        if (courseIds[courseID] && (instructors[msg.sender] || courses[courseID].TAs[msg.sender]) && courses[courseID].examIds[examID]) {
-            if (marksList.length != rollList.length)
-                return false;
-            for (uint256 i = 0; i < rollList.length; i++)
-                courses[courseID].exams[examID].marks[rollList[i]] = marksList[i];
-            return true;
-        }
-        else
-            return false;
+    function updateMarks(bytes32 courseID, bytes32 examID, bytes32[] rollList, uint[] marksList) public returns (bool added) {
+        require (courseIds[courseID] && (instructors[msg.sender] || courses[courseID].TAs[msg.sender]) && courses[courseID].examIds[examID], "updateMarks");
+        if (marksList.length != rollList.length)
+            added = false;
+        for (uint i = 0; i < rollList.length; i++)
+            courses[courseID].exams[examID].marks[rollList[i]] = marksList[i];
+        added = true;
     }
 
-    function setWeightages(bytes32 courseID, bytes32[] examIDList, uint256[] weightageList) public returns (bool added)  {
-        if (courseIds[courseID] && instructors[msg.sender]) {
-            if (examIDList.length != weightageList.length)
-                return false;
-            for (uint256 i = 0; i < examIDList.length; i++)
-                courses[courseID].weightage[examIDList[i]] = weightageList[i];
-            return true;
-        }
-        else
-            return false;
+    function setWeightages(bytes32 courseID, bytes32[] examIDList, uint[] weightageList) public returns (bool added) {
+        require (courseIds[courseID] && instructors[msg.sender], "setWeightages");
+        if (examIDList.length != weightageList.length)
+            added = false;
+        for (uint i = 0; i < examIDList.length; i++)
+            courses[courseID].weightage[examIDList[i]] = weightageList[i];
+        added = true;
     }
 
-    function setGradeCutoffs (bytes32 courseID, uint256[] gradeCutoffs) public returns (bool added) {
-        if (courseIds[courseID] && instructors[msg.sender]) {
-            string[8] memory gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
-            if (gradeCutoffs.length != (gradeList.length - 1))
-                return false;
-            courses[courseID].gradeCutoffs = gradeCutoffs;
-            return true;
-        }
-        else
-            return false;
+    function setGradeCutoffs(bytes32 courseID, uint[] gradeCutoffs) public returns (bool added) {
+        require (courseIds[courseID] && instructors[msg.sender], "setGradeCutoffs");
+        string[8] memory gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
+        if (gradeCutoffs.length != (gradeList.length - 1))
+            added = false;
+        courses[courseID].gradeCutoffs = gradeCutoffs;
+        added = true;
     }
 
     function calculateTotal (bytes32 courseID) public returns (bool added) {
-        if (courseIds[courseID] && instructors[msg.sender]) {
-            uint pres = 1000;
-            for (uint256 i = 0; i < courses[courseID].examIDList.length; i++) {
-                bytes32 exam_id = courses[courseID].examIDList[i];
-                uint256 maxmarks = courses[courseID].exams[exam_id].maxMarks;
-                uint256 weightage = courses[courseID].weightage[exam_id];
-                for (uint256 j = 0; j < courses[courseID].rollList.length; j++) {
-                      bytes32 roll_no = courses[courseID].rollList[j];
-                      courses[courseID].totalMarks[roll_no] += ((courses[courseID].exams[exam_id].marks[roll_no] * pres * weightage)/maxmarks);
-                }
+        require (courseIds[courseID] && instructors[msg.sender], "calculateTotal");
+        uint pres = 1000;
+        for (uint i = 0; i < courses[courseID].examIDList.length; i++) {
+            bytes32 exam_id = courses[courseID].examIDList[i];
+            uint maxmarks = courses[courseID].exams[exam_id].maxMarks;
+            uint weightage = courses[courseID].weightage[exam_id];
+            for (uint j = 0; j < courses[courseID].rollList.length; j++) {
+                  bytes32 roll_no = courses[courseID].rollList[j];
+                  courses[courseID].totalMarks[roll_no] += ((courses[courseID].exams[exam_id].marks[roll_no] * pres * weightage)/maxmarks);
             }
-            for (uint256 k = 0; k < courses[courseID].rollList.length; k++) {
-                  courses[courseID].totalMarks[courses[courseID].rollList[k]] = (courses[courseID].totalMarks[courses[courseID].rollList[k]]/pres);
-            }
-            return true;
         }
-        else
-            return false;
+        for (uint k = 0; k < courses[courseID].rollList.length; k++) {
+              courses[courseID].totalMarks[courses[courseID].rollList[k]] = (courses[courseID].totalMarks[courses[courseID].rollList[k]]/pres);
+        }
+        added = true;
     }
 
     function calculateGrades (bytes32 courseID) public returns (bool added) {
-      if (courseIds[courseID] && instructors[msg.sender]) {
-          string[8] memory gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
-          for (uint256 i = 0; i < courses[courseID].rollList.length; i++) {
-              for (uint256 j = 0; j < courses[courseID].gradeCutoffs.length; j++) {
-                    bytes32 roll_no = courses[courseID].rollList[i];
-                    if (courses[courseID].totalMarks[roll_no] >= courses[courseID].gradeCutoffs[j])
-                        courses[courseID].grades[roll_no] = gradeList[j];
-                        break;
-              }
-          }
-          return true;
-      }
-      else
-          return false;
+        require (courseIds[courseID] && instructors[msg.sender], "calculateGrades");
+        string[8] memory gradeList = ["AA", "AB", "BB", "BC", "CC", "CD", "DD", "FR"];
+        for (uint i = 0; i < courses[courseID].rollList.length; i++) {
+            for (uint j = 0; j < courses[courseID].gradeCutoffs.length; j++) {
+                bytes32 roll_no = courses[courseID].rollList[i];
+                if (courses[courseID].totalMarks[roll_no] >= courses[courseID].gradeCutoffs[j])
+                    courses[courseID].grades[roll_no] = gradeList[j];
+                    break;
+            }
+        }
+        added = true;
     }
 }
